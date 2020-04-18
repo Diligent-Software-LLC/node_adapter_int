9bc0601db6f873bedbf637a7c09da3f259dc990b
Bradley J. Tannor <bradleytannor@gmail.com>
2020-04-18 16:39:02 -0700

    Development Complete
    Added NodeAdapterInt 'back()', 'data()', 'front()', 'initialize(n = nil)', 'back=(n = nil)', 'data=(dti = nil)', and 'front=(n = nil)' method signatures. Added corresponding declaration tests.

--- lib/node_adapter_int.rb
+++ lib/node_adapter_int.rb
@@ -6,6 +6,80 @@ require_relative "node_adapter_int/version"
 # NodeAdapterInt.
 # @class_description
 #   A NodeAdapter interface.
+# @attr back [Node, NilCLass]
+#   A 'back' instance reference.
+# @attr data [DataType]
+#   A 'data' instance reference.
+# @attr front [Node, NilClass]
+#   A 'front' instance reference.
 class NodeAdapterInt
-end
 
+  # back().
+  # @description
+  #   Gets back's reference.
+  # @return [Node, NilClass]
+  #   back's reference.
+  def back()
+  end
+
+  # data().
+  # @description
+  #   Gets data's reference.
+  # @return [DataType]
+  #   data's reference.
+  def data()
+  end
+
+  # front().
+  # @description
+  #   Gets front's reference.
+  # @return [Node, NilClass]
+  #   front's reference.
+  def front()
+  end
+
+  protected
+
+  # initialize(n = nil).
+  # @description
+  #   Instantiates a NodeAdapter instance.
+  # @param n [Node]
+  #   The adapting instance.
+  # @return [Node]
+  #   The instance.
+  def initialize(n = nil)
+  end
+
+  private
+
+  # back=(n = nil).
+  # @description
+  #   Sets back's reference.
+  # @param n [Node, NilClass]
+  #   back's setting.
+  # @return [Node, NilClass]
+  #   The argument.
+  def back=(n = nil)
+  end
+
+  # data=(dti = nil).
+  # @description
+  #   Sets data's reference.
+  # @param dti [DataType]
+  #   data's setting.
+  # @return [DataType]
+  #   The argument.
+  def data=(dti = nil)
+  end
+
+  # front=(n = nil).
+  # @description
+  #   Sets front's reference.
+  # @param n [Node, NilClass]
+  #   front's setting.
+  # @return [Node, NilClass]
+  #   The argument.
+  def front=(n = nil)
+  end
+
+end
--- test/node_adapter_int_test.rb
+++ test/node_adapter_int_test.rb
@@ -2,13 +2,15 @@ require_relative 'test_helper'
 
 # NodeAdapterIntTest.
 # @class_description
-#   Tests the NodeAdapterInt class.
+#   Tests the NodeAdapter interface.
 class NodeAdapterIntTest < Minitest::Test
+
+  CLASS = NodeAdapterInt
 
   # test_conf_doc_f_ex().
   # @description
-  #  The .travis.yml, CODE_OF_CONDUCT.md, Gemfile, LICENSE.txt, README.md, 
-  #  and .yardopts files exist.
+  #   The .travis.yml, CODE_OF_CONDUCT.md, Gemfile, LICENSE.txt, README.md, and
+  #   .yardopts files exist.
   def test_conf_doc_f_ex()
 
     assert_path_exists('.travis.yml')
@@ -31,13 +33,41 @@ class NodeAdapterIntTest < Minitest::Test
   # @description
   #   Set fixtures.
   def setup()
+
+    @pub_i_m  = CLASS.public_instance_methods(false)
+    @prot_i_m = CLASS.protected_methods(false)
+    @priv_i_m = CLASS.private_instance_methods(false)
+
+  end
+
+  # test_pub_m_dec().
+  # @description
+  #   Methods 'back()', 'data()', and 'front()' were declared.
+  def test_pub_m_dec()
+
+    assert_includes(@pub_i_m, :back)
+    assert_includes(@pub_i_m, :data)
+    assert_includes(@pub_i_m, :front)
+
+  end
+
+  # test_priv_m_dec().
+  # @description
+  #   Methods 'initialize(n = nil)', 'back=(n = nil)', 'data=(dti = nil)', and
+  #   'front=(n = nil)' were declared.
+  def test_priv_m_dec()
+
+    assert_includes(@priv_i_m, :initialize)
+    assert_includes(@priv_i_m, :back=)
+    assert_includes(@priv_i_m, :data=)
+    assert_includes(@priv_i_m, :front=)
+
   end
 
   # teardown().
   # @description
-  #  Cleanup.
+  #   Cleanup.
   def teardown()
   end
 
 end
-
